e4b9d3c1f38c2c2ca34dded8fbe28f4e349ee81a
Lara Aschenbeck <larasche@uni-bremen.de>
2021-02-16 13:00:20 +0100

    Plots the SC depending on the angel

--- horizon.py
+++ horizon.py
@@ -22,6 +22,9 @@ NO2cross = np.loadtxt("Daten/NO2_DiffXSection.dat", skiprows=6)
 # reads the dark measurement in an array
 
 idark500 = np.loadtxt("Daten/abgedunkelt_500ms.DAT", skiprows=17)
+idark200 = np.loadtxt("Daten/abgedunkelt_200ms.DAT", skiprows=17)
+idark300 = np.loadtxt("Daten/abgedunkelt_300ms.DAT", skiprows=17)
+idark150 = idark300/2
 
 # reads the angular measurements
 filenames = os.listdir('Daten')
@@ -41,6 +44,7 @@ for key in i_measurements:
         if key == str(nn):
             i_darkcorrected[key] = i_measurements[key][:, 1] - idark500[:, 1]
 
+i0_darkcorrected = I0_measurement[:, 1] - idark200[:, 1]
 
 # apply wavelength calibration
 a0 = float(429.494)
@@ -71,3 +75,78 @@ for key in i_darkcorrected:
     for nn in range(0, 15):
         if key == str(nn):
             i_ready[key] = i_darkcorrected[key][wavemin: wavemax]
+
+i0_ready = i0_darkcorrected[wavemin: wavemax]
+
+
+# calculate log(I0_ready/I_ready)
+
+i_log = {}
+
+for key in i_ready:
+    i_log[key] = np.log(i0_ready/i_ready[key])
+
+
+
+def polynom3(a0, a1, a2, a3, x):
+    """"calculates the polynom of order 3"""
+    aa = a0*x**3 + a1*x**2 + a2*x + a3
+    return aa
+
+
+poly = {}
+for key in i_log:
+    fitparas = np.polyfit(wavelen, i_log[key], 3)
+    poly[key] = polynom3(fitparas[0], fitparas[1], fitparas[2], fitparas[3], wavelen)
+
+
+# substract the fitted polynomial from i_log to get the differential i_logdiff
+
+i_logdiff = {}
+for key in i_log:
+    for key2 in poly:
+        if key == key2:
+            i_logdiff[key] = i_log[key] - poly[key2]
+
+
+
+# interpolate the differential cross section to the measurement wavelengths
+waveair = NO2cross[:, 0]
+diffNO2 = NO2cross[:, 1]
+intercross = interp1d(waveair, diffNO2)  # interpolates the FUNKTION intercross
+
+# fit the cross section to mesurement wavelength (wavelen)
+
+finalcross = intercross(wavelen)  # finalcross = cross section for the right wavelength
+
+nfinalcross = np.reshape(finalcross, (367, 1))
+
+
+
+idiffnew = {}
+for key in i_logdiff:
+    for nn in range(0, 60):
+        if key == str(nn):
+            idiffnew[key] = np.reshape(i_logdiff[key], (367, -1))
+
+
+fit_SC = {}
+for key in idiffnew:
+    for nn in range(0, 60):
+        if key == str(nn):
+            fit_SC[key] = nl.lstsq(nfinalcross, idiffnew[key])
+
+
+plt.plot(wavelen, i_logdiff["5"], "-", color="red")
+plt.plot(wavelen, fit_SC["5"][0]*nfinalcross, ".", color="blue")
+plt.show
+
+SC = []
+angel = []
+for key in fit_SC:
+    SC.append(float(fit_SC[key][0]))
+    angel.append(int(key))
+
+
+plt.plot(angel, SC, ".")
+plt.show()
